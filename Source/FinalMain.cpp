4bfcc687e7bac2648f6e3835afefe3ea588eb558
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-11-11 22:40:52 -0700

    Area partially mapped
    Trees still need performance optimization, lighting needs to be enabled and tested in the full environment, half of the area still needs to be populated with objects.

--- Source/FinalMain.cpp
+++ Source/FinalMain.cpp
@@ -1,3 +1,8 @@
+/*
+ *   Final Project Main Entry Point.
+ *   Created by Jeff Colgan, October 26, 2021,
+ *   CSCI-4229 Computer Graphics: Final Project.
+ */
 #include "CSCIx229.h"
 #include "Cuboid.h"
 #include "Camera.h"
@@ -8,71 +13,131 @@
 #include "Skybox.h"
 #include <vector>
 
-Cuboid ground;
-Camera player;
+// Variables for graphical objects.
+Cuboid* ground;
+Camera* player;
 std::vector<Tree *> tree;
 Shovel* shovel;
 Axe* axe;
 Cabin* cabin;
 Skybox* sky;
 
+const int treeCount = 40;
+
+// Initialize game objects.
+void initialize_objects()
+{
+   // Coordinates for Tree objects.
+   float treeCoord[treeCount][2] = { {-43.0, -20.0}, {-44.0, -10.0}, {-45.0, -2.0},
+                                     {-44.0, 5.0}, {-42.0, 13.0}, {-45.0, 21.0},
+                                     {-46.0, 30.0}, {-39.0, 28.0}, {-42.0, 35.0},
+                                     {-37.0, -17.0}, {-35.0, -8.0}, {-38.0, 2.0},
+                                     {-34.0, 10.0}, {-36.0, 22.0}, {-28.0, -45.0},
+                                     {-26.0, -38.0}, {-25.0, -29.0}, {-29.0, -17.0},
+                                     {-27.0, -4.0}, {-28.0, 6.0}, {-26.0, 18.0},
+                                     {-29.0, 26.0}, {-27.0, 33.0}, {-19.0, -44.0},
+                                     {-17.0, -34.0}, {-24.0, -20.0}, {-18.0, -11.0},
+                                     {-15.0, -3.0}, {-17.0, 7.0}, {-19.0, 16.0},
+                                     {-17.0, 24.0}, {-5.0, -44.0}, {-7.0, -38.0},
+                                     {-4.0, -29.0}, {-11.0, -25.0}, {-6.0, -14.0},
+                                     {-12.0, -17.0}, {-5.0, -4.0}, {-4.0, 7.0}, {-7.0, 18.0} };
+
+   // Instantiate Trees.
+   for (int i = 0; i < treeCount; i++)
+   {
+      tree.push_back(new Tree(treeCoord[i][0], 1.0, treeCoord[i][1], 1.0, 1.0, 1.0, 0.0, 0.0, 0.0));
+      tree.at(i)->Initialize("Assets/Bark.bmp");
+   }
+
+   // Instantiate ground, camera.
+   player = new Camera();
+   ground = new Cuboid(0.0, 0.0, 0.0, 50.0, 1.0, 50.0, 0.0, 0.0, 0.0);
+   ground->Initialize("Assets/Dirt.bmp");
+
+   // Instantiate skybox.
+   sky = new Skybox(0.0, 0.0, 0.0, 50.0, 50.0, 50.0, 0.0, 0.0, 0.0);
+   sky->Initialize("Assets/Stars.bmp");
+
+   // Instantiate shovel, axe.
+   shovel = new Shovel(-15.0, 2.0, -15.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0);
+   shovel->Initialize("Assets/Wood.bmp");
+   axe = new Axe(-15.0, 1.5, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0);
+   axe->Initialize("Assets/Wood.bmp");
+
+   // Instantiate Cabin.
+   cabin = new Cabin(-42.0, 3.0, -39.0, 5.0, 2.0, 7.0, 0.0, 0.0, 0.0);
+   cabin->Initialize("Assets/Bricks.bmp");
+}
+
+// Display function, called by GLUT to update the screen.
 void display()
 {
-   // Call object render functions here.
+   // Clear the buffers, enable depth test.
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    glEnable(GL_DEPTH_TEST);
 
+   // Clear transformations and apply camera movement.
    glLoadIdentity();
-
-   player.Turn();
-   gluLookAt(player.getEyeX(), player.getEyeY(), player.getEyeZ(), player.getCenterX() + player.getEyeX(), player.getCenterY(), player.getCenterZ() + player.getEyeZ(), player.getUpX(), player.getUpY(), player.getUpZ());
+   player->Turn();
+   gluLookAt(player->getEyeX(), player->getEyeY(), player->getEyeZ(), player->getCenterX() + player->getEyeX(), player->getCenterY(), player->getCenterZ() + player->getEyeZ(), player->getUpX(), player->getUpY(), player->getUpZ());
    
+   // Enable normals, lighting.
    glEnable(GL_NORMALIZE);
 
-   for (int i = 0; i < 10; i++)
+   // Render Tree objects.
+   for (int i = 0; i < treeCount; i++)
       tree.at(i)->Render();
 
-   ground.Render();
+   // Render other objects.
+   ground->Render();
    shovel->Render();
    axe->Render();
    cabin->Render();
    sky->Render();
 
+   // Check for errors in GLUT, flush and swap buffers.
    ErrCheck("display");
    glFlush();
    glutSwapBuffers();
 }
 
+// Special function, called by GLUT when the user presses an arrow key.
 void special(int key, int x, int y)
 {
+   // If the player presses up arrow, move camera forward.
    if(key == GLUT_KEY_UP)
    {
-      player.MoveForward();
+      player->MoveForward();
    }
+   // If the player presses down arrow, move camera backward.
    else if (key == GLUT_KEY_DOWN)
    {
-      player.MoveBackward();
+      player->MoveBackward();
    }
+   // If the player presses right arrow, turn camera right.
    else if (key == GLUT_KEY_RIGHT)
    {
-      int theta = player.getTheta() + 5;
+      int theta = player->getTheta() + 5;
       theta %= 360;
-      player.setTheta(theta);
+      player->setTheta(theta);
    }
+   // If the player presses left arrow, turn camera left.
    else if (key == GLUT_KEY_LEFT)
    {
-      int theta = player.getTheta() - 5;
+      int theta = player->getTheta() - 5;
       theta %= 360;
-      player.setTheta(theta);
+      player->setTheta(theta);
    }
 
-   Project(45, player.getAsp(), player.getDim());
-
+   // Set projection and update display.
+   Project(45, player->getAsp(), player->getDim());
    glutPostRedisplay();
 }
 
+// Key function, called by GLUT when the player presses a key.
 void key(unsigned char key, int x, int y)
 {
+   // If the player presses the 'escape' key, exit the application.
    if (key == 27)
    {
       exit(0);
@@ -81,20 +146,25 @@ void key(unsigned char key, int x, int y)
    glutPostRedisplay();
 }
 
+// Reshape function, called by GLUT when the application window is resized.
 void reshape(int width, int height)
 {
    double asp;
    
+   // Specify viewport, set new aspect ratio, set projection.
    glViewport(0, 0, RES*width, RES*height);
    asp = (height > 0) ? (double)width/height : 1;
-   player.setAsp(asp);
-   Project(45, player.getAsp(), player.getDim());
+   player->setAsp(asp);
+   Project(45, player->getAsp(), player->getDim());
 }
 
+// Main entry point.
 int main(int argc, char* argv[])
 {
+   // Initialize GLUT.
    glutInit(&argc, argv);
 
+   // Create 500 X 500 window, enable true color RGB, depth buffer, double buffering, name window.
    glutInitWindowSize(500, 500);
    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
    glutCreateWindow("Jeff Colgan");
@@ -103,34 +173,16 @@ int main(int argc, char* argv[])
    if(glewInit() != GLEW_OK) Fatal("Error initializing GLEW\n");
 #endif
 
+   // Specify displaym reshape, special and key functions.
    glutDisplayFunc(display);
    glutReshapeFunc(reshape);
    glutSpecialFunc(special);
    glutKeyboardFunc(key);
 
-   ground = Cuboid(0.0, 0.0, 0.0, 50.0, 1.0, 50.0, 0.0, 0.0, 0.0);
-   ground.Initialize("Assets/Dirt.bmp");
+   // Initialize graphical objects.
+   initialize_objects();
 
-   player = Camera();
-
-   for (int i = 0; i < 10; i++)
-   {
-      tree.push_back(new Tree(i*10.0, 1.0, i*10.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0));
-      tree.at(i)->Initialize("Assets/Bark.bmp");
-   }
-
-   shovel = new Shovel(-15.0, 3.0, -15.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0);
-   shovel->Initialize("Assets/Wood.bmp");
-
-   axe = new Axe(-15.0, 0.5, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0);
-   axe->Initialize("Assets/Wood.bmp");
-
-   cabin = new Cabin(10.0, 3.0, 0.0, 6.0, 2.0, 9.0, 0.0, 180.0, 0.0);
-   cabin->Initialize("Assets/Bricks.bmp");
-
-   sky = new Skybox(0.0, 0.0, 0.0, 50.0, 50.0, 50.0, 0.0, 0.0, 0.0);
-   sky->Initialize("Assets/Stars.bmp");
-
+   // Check for errors and pass control to GLUT.
    ErrCheck("init");
    glutMainLoop();
 
