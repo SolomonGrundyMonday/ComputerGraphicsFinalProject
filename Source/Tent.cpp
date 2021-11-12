6a850ae7ea34d3f885f31ab75282ea3f607f38b7
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-11-12 13:08:56 -0700

    Tent mostly finished, still need spikes, tethers

--- Source/Tent.cpp
+++ Source/Tent.cpp
@@ -36,11 +36,14 @@ Tent::Tent(float x, float y, float z, float dx, float dy, float dz, float rx, fl
 int Tent::Initialize(const char* filename)
 {
    this->texture = LoadTexBMP(filename);
+   return 0;
 }
 
 // Function definition for Tent object Render function implementation.
 void Tent::Render()
 {
+   float textureRatio = 1.0/12.0;
+
    // Enable textures.
    glEnable(GL_TEXTURE_2D);
    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
@@ -55,11 +58,210 @@ void Tent::Render()
    glRotatef(this->rotZ, 0, 0, 1);
    glScalef(this->scaleX, this->scaleY, this->scaleZ);
 
-   // Draw Tent walls.
+   // Draw Tent front wall.
+   glNormal3f(-0.8, 0.0, 1.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(-1.0, -1.0, 1.0);
+   glTexCoord2f(this->scaleX, 0.0);
+   glVertex3f(-0.4, -1.0, 1.0);
+   glTexCoord2f(this->scaleX, this->scaleY);
+   glVertex3f(-0.4, 1.0, 1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(-1.0, 1.0, 1.0);
+   glEnd();
 
-   // Draw Tent flaps.
+   glNormal3f(0.8, 0.0, 1.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(0.4, -1.0, 1.0);
+   glTexCoord2f(this->scaleX, 0.0);
+   glVertex3f(1.0, -1.0, 1.0);
+   glTexCoord2f(this->scaleX, this->scaleY);
+   glVertex3f(1.0, 1.0, 1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(0.4, 1.0, 1.0);
+   glEnd();
+
+   glNormal3f(0.0, 0.6, 1.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(-0.4, 0.2, 1.0);
+   glTexCoord2f(this->scaleX, 0.0);
+   glVertex3f(0.4, 0.2, 1.0);
+   glTexCoord2f(this->scaleX, this->scaleY);
+   glVertex3f(0.4, 1.0, 1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(-0.4, 1.0, 1.0);
+   glEnd();
+
+   // Draw Tent back wall.
+   glNormal3f(0.0, 0.0, -1.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(1.0, -1.0, -1.0);
+   glTexCoord2f(this->scaleX, 0.0);
+   glVertex3f(-1.0, -1.0, -1.0);
+   glTexCoord2f(this->scaleX, this->scaleY);
+   glVertex3f(-1.0, 1.0, -1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(1.0, 1.0, -1.0);
+   glEnd();
+
+   // Draw Tent left wall.
+   glNormal3f(-0.5, 0.0, 0.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(-1.0, -1.0, -1.0);
+   glTexCoord2f(this->scaleZ, 0.0);
+   glVertex3f(-1.0, -1.0, 1.0);
+   glTexCoord2f(this->scaleZ, this->scaleY);
+   glVertex3f(-1.0, 1.0, 1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(-1.0, 1.0, -1.0);
+   glEnd();
+
+   // Draw Tent Right wall.
+   glNormal3f(0.5, 0.0, 0.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(1.0, -1.0, 1.0);
+   glTexCoord2f(this->scaleZ, 0.0);
+   glVertex3f(1.0, -1.0, -1.0);
+   glTexCoord2f(this->scaleZ, this->scaleY);
+   glVertex3f(1.0, 1.0, -1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(1.0, 1.0, 1.0);
+   glEnd();
 
    // Draw Tent roof.
+   glNormal3f(0.0, (1.5/2.0), 1.0);
+   glBegin(GL_TRIANGLES);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(-1.0, 1.0, 1.0);
+   glTexCoord2f(this->scaleX, 0.0);
+   glVertex3f(1.0, 1.0, 1.0);
+   glTexCoord2f(this->scaleX/2.0, (0.5 * this->scaleY)/2.0);
+   glVertex3f(0.0, 1.5, 1.0);
+   glEnd();
+
+   glNormal3f(0.0, 1.5/2.0, -1.0);
+   glBegin(GL_TRIANGLES);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(1.0, 1.0, -1.0);
+   glTexCoord2f(this->scaleX, 0.0);
+   glVertex3f(-1.0, 1.0, -1.0);
+   glTexCoord2f(this->scaleX/2.0, (0.5 * this->scaleY)/2.0);
+   glVertex3f(0.0, 1.5, -1.0);
+   glEnd();
+
+   glNormal3f(-0.5, 1.25, 0.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(-1.0, 1.0, -1.0);
+   glTexCoord2f(this->scaleZ, 0.0);
+   glVertex3f(-1.0, 1.0, 1.0);
+   glTexCoord2f(this->scaleZ, this->scaleY);
+   glVertex3f(0.0, 1.5, 1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(0.0, 1.5, -1.0);
+   glEnd();
+
+   glNormal3f(0.5, 1.25, 0.0);
+   glBegin(GL_QUADS);
+   glTexCoord2f(0.0, 0.0);
+   glVertex3f(1.0, 1.0, 1.0);
+   glTexCoord2f(this->scaleZ, 0.0);
+   glVertex3f(1.0, 1.0, -1.0);
+   glTexCoord2f(this->scaleZ, this->scaleY);
+   glVertex3f(0.0, 1.5, -1.0);
+   glTexCoord2f(0.0, this->scaleY);
+   glVertex3f(0.0, 1.5, 1.0);
+   glEnd();
+
+   // Draw Tent flaps.
+   glBegin(GL_QUAD_STRIP);
+   for (int i = 0; i <= 12; i++)
+   {
+      int theta = i * 30;
+      float x = Cos(theta);
+      float z = Sin(theta);
+
+      glNormal3f(x, -1.0, z);
+      glTexCoord2f(0.0, i * textureRatio);
+      glVertex3f(x * 0.1 + 0.4, -1.0, z * 0.1 + 1.0);
+      glTexCoord2f(12.0, i * textureRatio);
+      glVertex3f(x * 0.1 + 0.4, 0.2, z * 0.1 + 1.0);
+   }
+   glEnd();
+
+   glNormal3f(0.4, 0.2, 1.0);
+   glBegin(GL_TRIANGLE_FAN);
+   glTexCoord2f(0.5, 0.5);
+   glVertex3f(0.4, 0.2, 1.0);
+   for (int i = 0; i <= 360; i += 30)
+   {
+      glNormal3f(Cos(i) + 0.4, 0.2, Sin(i) + 1.0);
+      glTexCoord2f(0.5 * Cos(i) + 0.5, 0.5 * Sin(i) + 0.5);
+      glVertex3f(0.1 * Cos(i) + 0.4, 0.2, 0.1 * Sin(i) + 1.0);
+   }
+   glEnd();
+
+   glNormal3f(0.4, -1.0, 1.0);
+   glBegin(GL_TRIANGLE_FAN);
+   glTexCoord2f(0.5, 0.5);
+   glVertex3f(0.4, -1.0, 1.0);
+   for (int i = 0; i <= 360; i += 30)
+   {
+      glNormal3f(Cos(i) + 0.4, -1.0, Sin(i) + 1.0);
+      glTexCoord2f(0.5 * Cos(i) + 0.5, 0.5 * Sin(i) + 0.5);
+      glVertex3f(0.1 * Cos(i) + 0.4, -1.0, 0.1 * Sin(i) + 1.0);
+   }
+   glEnd();
+   
+   glBegin(GL_QUAD_STRIP);
+   for (int i = 0; i <= 12; i++)
+   {
+      int theta = i * 30;
+      float x = Cos(theta);
+      float z = Sin(theta);
+
+      glNormal3f(x, -1.0, z);
+      glTexCoord2f(0.0, i * textureRatio);
+      glVertex3f(x * 0.1 - 0.4, -1.0, z * 0.1 + 1.0);
+      glTexCoord2f(12.0, i * textureRatio);
+      glVertex3f(x * 0.1 - 0.4, 0.2, z * 0.1 + 1.0);
+   }
+   glEnd();
+
+   glNormal3f(-0.4, 0.2, 1.0);
+   glBegin(GL_TRIANGLE_FAN);
+   glTexCoord2f(0.5, 0.5);
+   glVertex3f(-0.4, 0.2, 1.0);
+   for (int i = 0; i <= 360; i += 30)
+   {
+      glNormal3f(Cos(i) - 0.4, 0.2, Sin(i) + 1.0);
+      glTexCoord2f(0.5 * Cos(i) + 0.5, 0.5 * Sin(i) + 0.5);
+      glVertex3f(0.1 * Cos(i) - 0.4, 0.2, 0.1 * Sin(i) + 1.0);
+   }
+   glEnd();
+
+   glNormal3f(-0.4, -1.0, 1.0);
+   glBegin(GL_TRIANGLE_FAN);
+   glTexCoord2f(0.5, 0.5);
+   glVertex3f(-0.4, -1.0, 1.0);
+   for (int i = 0; i <= 360; i += 30)
+   {
+      glNormal3f(Cos(i) - 0.4, -1.0, Sin(i) + 1.0);
+      glTexCoord2f(0.5 * Cos(i) + 0.5, 0.5 * Sin(i) + 0.5);
+      glVertex3f(0.1 * Cos(i) - 0.4, -1.0, 0.1 * Sin(i) + 1.0);
+   }
+   glEnd();
+
+   // Draw Tent spikes.
+   
+
+   // Draw Tent ties.
 
    glPopMatrix();
    glDisable(GL_TEXTURE_2D);
