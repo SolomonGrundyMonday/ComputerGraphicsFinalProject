db4b74d84e67a15f544e608287468164858ef129
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-11-28 20:13:17 -0700

    Implemented CaveWall object

--- Source/FinalMain.cpp
+++ Source/FinalMain.cpp
@@ -14,6 +14,7 @@
 #include "Lantern.h"
 #include "Horizon.h"
 #include "TreeStump.h"
+#include "CaveWall.h"
 #include <vector>
 
 // Variables for graphical objects.
@@ -26,11 +27,16 @@ Cabin* cabin;
 Skybox* sky;
 Lantern* lantern;
 Horizon* horizon;
-TreeStump* stump;
+CaveWall* back;
+CaveWall* left;
+CaveWall* right;
+CaveWall* front;
+std::vector<TreeStump *> stump;
 std::vector<Tent *> tent;
 
-const int treeCount = 49;
+const int treeCount = 47;
 const int tentCount = 4;
+const int stumpCount = 8;
 
 // Initialize game objects.
 void initialize_objects()
@@ -38,23 +44,28 @@ void initialize_objects()
    // Coordinates for Tree objects.
    float treeCoord[treeCount][2] = { {-27.0, -8.5}, {-16.0, -14.0}, {-18.0, -10.0},
                                      {-15.0, -3.0}, {-16.0, 4.0}, {-19.0, 9.0},
-                                     {-17.0, 15.0}, {-15.0, 22.0}, {10.5, -22.0},
+                                     {-17.0, 15.0}, {-15.0, 22.0}, {8.0, -22.0},
                                      {-22.0, -3.0}, {-23.0, 6.0}, {-22.0, 19.0},
                                      {3.0, -10.0}, {-9.0, -25.5}, {-14.0, -19.5},
-                                     {-11.0, -12.0}, {-10.0, -6.0}, {-11.0, 1.5},
+                                     {-11.0, -12.5}, {-10.0, -6.0}, {-11.0, 1.5},
                                      {-12.0, 7.0}, {-10.5, 13.0}, {-11.5, 19.0},
-                                     {-4.0, -23.0}, {-2.0, -16.5}, {-3.0, -10.0},
-                                     {-6.0, -1.5}, {-4.0, 6.5}, {-5.0, 13.0},
+                                     {-4.0, -23.0}, {-2.0, -14.5}, {-3.0, -10.0},
+                                     {-7.0, 0.5}, {-4.0, 6.5}, {-5.0, 13.0},
                                      {-3.0, 17.0}, {-6.0, 23.0}, {-25.5, 11.5},
-									 {-1.0, -4.5}, {10.0, 1.0}, {0.0, 11.5},
-                                     {7.0, -13.5}, {4.0, -3.0}, {11.0, -6.5},
-                                     {3.0, 7.0}, {5.0, 14.5}, {4.0, 21.0},
-                                     {-11.0, -23.5}, {12.0, -15.5}, {-7.0, -18.0},
-                                     {6.0, -24.5}, {-21.0, 26.0}, {4.0, -18.5},
-                                     {-8.0, 4.5}, {-6.0, -7.0}, {-18.5, 1.0} };
+									 {-1.0, -4.5}, {9.0, 1.0}, {0.0, 11.5},
+                                     {6.5, -11.0}, {4.0, -3.0}, {9.5, -6.5},
+                                     {2.5, 6.5}, {-6.0, -7.0}, {-18.5, 1.0},
+                                     {-11.5, -23.5}, {10.0, -15.5}, {-7.0, -18.0},
+                                     {4.0, -24.5}, {-21.0, 26.0}, {3.0, -18.5},
+                                     {-8.0, 4.5} };
 
    // Coordinates for Tent objects.
    float tentCoord[tentCount][2] = { {15.0, 10.0}, {15.0, 20.0}, {20.0, 10.0}, {20.0, 20.0} };
+
+   // Coordinates for TreeStump objects.
+   float stumpCoord[stumpCount][2] = { {5.0, 14.5}, {18.5, 15.0}, {4.0, 21.0},
+                                       {3.5, 9.5}, {1.0, 17.0}, {6.5, 18.0},
+                                       {9.0, 14.0}, {10.5, 7.5} };
 
    // Instantiate Trees.
    for (int i = 0; i < treeCount; i++)
@@ -68,6 +79,13 @@ void initialize_objects()
    {
       tent.push_back(new Tent(tentCoord[i][0], 1.8, tentCoord[i][1], 1.0, 0.8, 1.0, 0.0, (i % 2) * 180.0, 0.0));
       tent.at(i)->Initialize("Assets/Canvas.bmp");
+   }
+
+   // Instantiate TreeStumps.
+   for (int i = 0; i < stumpCount; i++)
+   {
+      stump.push_back(new TreeStump(stumpCoord[i][0], 1.0, stumpCoord[i][1], 0.1, 0.2, 0.1, 0.0, 0.0, 0.0));
+      stump.at(i)->Initialize("Assets/Bark.bmp");
    }
 
    // Instantiate ground, camera.
@@ -84,10 +102,10 @@ void initialize_objects()
    horizon->Initialize("Assets/Horizon.bmp");
 
    // Instantiate shovel, axe.
-   shovel = new Shovel(17.5, 1.35, 15.0, 0.25, 0.25, 0.25, 15.0, 0.0, 0.0);
+   shovel = new Shovel(18.5, 1.25, 14.85, 0.25, 0.25, 0.25, 55.0, 0.0, 0.0);
    shovel->Initialize("Assets/Wood.bmp");
 
-   axe = new Axe(-25.2, 1.0, -20.8, 0.4, 0.4, 0.4, -30.0, 0.0, 0.0);
+   axe = new Axe(9.5, 1.45, 14.0, 0.4, 0.4, 0.4, 0.0, 0.0, 120.0);
    axe->Initialize("Assets/Wood.bmp");
 
    // Instantiate Cabin.
@@ -95,11 +113,21 @@ void initialize_objects()
    cabin->Initialize("Assets/Bricks.bmp");
 
    // Instantiate Lantern.
-   lantern = new Lantern(-23.0, 1.5, -13.5, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
+   lantern = new Lantern(18.5, 1.3, 15.0, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
    lantern->Initialize("Assets/RustyMetal.bmp");
 
-   stump = new TreeStump(18.5, 1.0, 15.0, 0.1, 0.2, 0.1, 0.0, 0.0, 0.0);
-   stump->Initialize("Assets/Bark.bmp");
+   // Instatiate CaveWalls.
+   back = new CaveWall(19.0, 1.0, -18.0, 3.0, 2.0, 1.0, 0.0, 0.0, 0.0);
+   back->Initialize("Assets/Rocks.bmp");
+
+   left = new CaveWall(17.0, 1.0, -9.0, 10.0, 2.0, 1.0, 0.0, 90.0, 0.0);
+   left->Initialize("Assets/Rocks.bmp");
+
+   right = new CaveWall(21.0, 1.0, -9.0, 10.0, 2.0, 1.0, 0.0, -90.0, 0.0);
+   right->Initialize("Assets/Rocks.bmp");
+
+   front = new CaveWall(20.0, 1.0, 0.0, 2.0, 2.0, 1.0, 0.0, 180.0, 0.0);
+   front->Initialize("Assets/Rocks.bmp");
 }
 
 // Display function, called by GLUT to update the screen.
@@ -128,28 +156,35 @@ void display()
    glLoadIdentity();
    player->Turn();
 
-   // Resolve object collisions.
+   // Resolve Horizon collisions (handles collisions for skybox).
    if (horizon->detectCollision(player))
       horizon->resolveCollision(player);
 
+   // Resolve collisions for Cabin.
    cabin->resolveCollision(player);
+
+   // Resolve collisions for Axe.
    if (axe->detectCollision(player))
       axe->resolveCollision(player);
 
+   // Resolve collisions for Shovel.
    if (shovel->detectCollision(player))
       shovel->resolveCollision(player);
 
-   if (lantern->detectCollision(player))
-      lantern->resolveCollision(player);
+   // Resolve collisions for TreeStumps.
+   for (int i = 0; i < stumpCount; i++)
+   {
+      if (stump.at(i)->detectCollision(player))
+         stump.at(i)->resolveCollision(player);
+   }
 
-   if (stump->detectCollision(player))
-      stump->resolveCollision(player);
-
+   // Resolve collisions for Trees.
    for (int i = 0; i < treeCount; i++)
    {
       tree.at(i)->resolveCollision(player);
    }
 
+   // Resolve collisions for Tents.
    for (int i = 0; i < tentCount; i++)
    {
       if (tent.at(i)->detectCollision(player))
@@ -158,6 +193,16 @@ void display()
          tent.at(i)->resolveCollision(player);
       }
    }
+
+   // Resolve collisions for CaveWalls.
+   if (back->detectCollision(player))
+      back->resolveCollision(player);
+   if (left->detectCollision(player))
+      left->resolveCollision(player);
+   if (right->detectCollision(player))
+      right->resolveCollision(player);
+   if (front->detectCollision(player))
+      front->resolveCollision(player);
 
    // Update Camera center vector based on player movement.
    player->setCenterPos(Eye[0] + player->getCenterX(), player->getCenterY(), Eye[2] + player->getCenterZ());
@@ -172,19 +217,39 @@ void display()
    for (int i = 0; i < treeCount; i++)
       tree.at(i)->Render();
 
+   // Render Tent objects.
    for (int i = 0; i < tentCount; i++)
       tent.at(i)->Render();
 
-   // Render other objects.
+   // Render TreeStump objects.
+   for (int i = 0; i < stumpCount; i++)
+      stump.at(i)->Render();
+
+   // Render the ground.
    ground->Render();
+
+   // Render Shovel object.
    shovel->Render();
+
+   // Render Axe object.
    axe->Render();
+
+   // Render Cabin object.
    cabin->Render();
+
+   // Render Skybox/Horizon objects.
    sky->Render();
    horizon->Render();
+
+   // Render Lantern, enable Lantern light source.
    lantern->Render();
    lantern->lightSource();
-   stump->Render();
+
+   // Render CaveWall objects.
+   back->Render();
+   left->Render();
+   right->Render();
+   front->Render();
 
    glWindowPos2i(5, 5);
    Print("Current position: (%.1lf, %.1lf, %.1lf)", player->getEyeX(), player->getEyeY(), player->getEyeZ());
